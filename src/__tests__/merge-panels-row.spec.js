d619c766096bf2b5e1bbbf07df4e9ef09ae7ca4a
Simon Mollweide <simon.mollweide@namics.com>
2017-10-21 23:29:04 +0200

    test(snapshots): Try fixing snapshot problems because of ANSI escape codes

--- src/__tests__/merge-panels-row.spec.js
+++ src/__tests__/merge-panels-row.spec.js
@@ -4,25 +4,30 @@ const mergePanelsRow = require('../merge-panels-row');
 
 describe('mergePanelsRow', () => {
 	it('default', () => {
-		const panel = JSON.parse(fs.readFileSync('./src/__tests__/__snapshots__/get-terminal-panel-1.json', 'utf8'))
-			.terminalPanel;
-		const result = JSON.stringify({
-			merged: mergePanelsRow([panel, panel], panel.length),
-		});
-		// fs.writeFileSync('./src/__tests__/__snapshots__/merge-panels-row-1.json', result);
-		const expected = fs.readFileSync('./src/__tests__/__snapshots__/merge-panels-row-1.json', 'utf8');
+		const panel = JSON.parse(
+			decodeURI(fs.readFileSync('./src/__tests__/__snapshots__/get-terminal-panel-1', 'utf8'))
+		).terminalPanel;
+		const result = encodeURI(
+			JSON.stringify({
+				merged: mergePanelsRow([panel, panel], panel.length),
+			})
+		);
+		// fs.writeFileSync('./src/__tests__/__snapshots__/merge-panels-row-1', result);
+		const expected = fs.readFileSync('./src/__tests__/__snapshots__/merge-panels-row-1', 'utf8');
 		expect(result).toEqual(expected);
 	});
 	it('un equal line length', () => {
-		const panel1 = JSON.parse(fs.readFileSync('./src/__tests__/__snapshots__/get-terminal-panel-1.json', 'utf8'))
-			.terminalPanel;
-		const panel2 = JSON.parse(fs.readFileSync('./src/__tests__/__snapshots__/get-terminal-panel-2.json', 'utf8'))
-			.terminalPanel;
+		const panel1 = JSON.parse(
+			decodeURI(fs.readFileSync('./src/__tests__/__snapshots__/get-terminal-panel-1', 'utf8'))
+		).terminalPanel;
+		const panel2 = JSON.parse(
+			decodeURI(fs.readFileSync('./src/__tests__/__snapshots__/get-terminal-panel-2', 'utf8'))
+		).terminalPanel;
 		const result = JSON.stringify({
 			merged: mergePanelsRow([panel1, panel2], panel1.length),
 		});
-		// fs.writeFileSync('./src/__tests__/__snapshots__/merge-panels-row-2.json', result);
-		const expected = fs.readFileSync('./src/__tests__/__snapshots__/merge-panels-row-2.json', 'utf8');
+		// fs.writeFileSync('./src/__tests__/__snapshots__/merge-panels-row-2', result);
+		const expected = fs.readFileSync('./src/__tests__/__snapshots__/merge-panels-row-2', 'utf8');
 		expect(result).toEqual(expected);
 	});
 });
