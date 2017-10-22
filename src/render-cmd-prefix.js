1641dd58371df7c3d60c8ca561006992979b9cd2
Simon Mollweide <simon.mollweide@namics.com>
2017-10-22 13:04:46 +0200

    feat(cmd prefix): Display pwd in command line eg. "lerna-terminal/<pwd>~$"

--- src/render-cmd-prefix.js
+++ src/render-cmd-prefix.js
@@ -1,13 +1,18 @@
 'use strict';
 
-const cmdPrefix = 'lerna-terminal~$ ';
+const cmdPrefix = cmd => `lerna-terminal${cmd ? `/${cmd}` : ''}~$ `;
+const { uiState } = require('./store');
 
 /**
  * @description provides an string in terminal with cursor afterwards
  * @returns {void}
 **/
 function renderCmdPrefix() {
-	process.stdout.write(cmdPrefix);
+	if (typeof uiState.focus === 'string' && uiState.focus !== '' && uiState.focus !== 'all') {
+		process.stdout.write(cmdPrefix(uiState.focus));
+		return;
+	}
+	process.stdout.write(cmdPrefix());
 }
 
 module.exports = renderCmdPrefix;
