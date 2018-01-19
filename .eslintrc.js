55d557d2b56aa168b277f5051763545a0c069579
Simon Mollweide <simon.mollweide@namics.com>
2018-01-19 23:30:19 +0100

    fix(dependencies): add missing dependencies
    #1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
 		'@namics/eslint-config/configurations/es6-node.js',
 		'@namics/eslint-config/configurations/es6-node-disable-styles.js',
 	],
+	rules: {
+		complexity: 0,
+	},
 	globals: {
 		describe: true,
 		it: true,
