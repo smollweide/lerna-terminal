8b71a4dffe127a32e6002dadc7bd4af6889a3d26
Simon Mollweide <simon.mollweide@namics.com>
2018-04-23 08:46:27 +0200

    fix(command-listener): fix stdin and stdout handling

--- src/commandListener/index.js
+++ src/commandListener/index.js
@@ -1,4 +1,10 @@
 'use strict';
+const keypress = require('keypress');
+const getFilledArray = require('../getFilledArray');
+
+keypress(process.stdin);
+
+let buffer = '';
 
 /**
  * @param {Function<string>} onCommandEntered - the callback function
@@ -6,17 +12,33 @@
  **/
 function commandListener(onCommandEntered) {
 	process.stdin.setEncoding('utf8');
-	process.stdin.on('readable', () => {
-		const chunk = process.stdin.read();
-		if (chunk !== null) {
-			onCommandEntered(
-				chunk
-					.trim()
-					.replace(/\n/g, '')
-					.replace(/\t/g, '')
-			);
+	process.stdin.setRawMode(true);
+
+	process.stdin.on('keypress', (letter, key) => {
+		if (key && key.ctrl && key.name === 'c') {
+			buffer = '';
+			process.exit();
+		} else if (key && key.name === 'backspace') {
+			process.stdout.write(getFilledArray(buffer.length, '\b').join(''));
+			buffer = buffer.substring(0, buffer.length - 1);
+			process.stdout.write(buffer);
+		} else if (key && key.name === 'return') {
+			if (buffer.length > 0) {
+				onCommandEntered(
+					buffer
+						.trim()
+						.replace(/\n/g, '')
+						.replace(/\t/g, '')
+				);
+			}
+			buffer = '';
+		} else if (letter) {
+			process.stdout.write(getFilledArray(buffer.length, '\b').join(''));
+			buffer += letter;
+			process.stdout.write(buffer);
 		}
 	});
+	process.stdin.resume();
 }
 
 module.exports = commandListener;
--- src/commandListener/index.spec.js
+++ src/commandListener/index.spec.js
@@ -5,20 +5,52 @@ const commandListener = require('./index');
 const _process = {
 	stdin: {
 		setEncoding: jest.fn(),
+		setRawMode: jest.fn(),
+		resume: jest.fn(),
 		on: jest.fn(),
 		read() {
 			return 'test';
 		},
 	},
+	stdout: {
+		write: jest.fn(),
+	},
+};
+
+const addString = inValue => {
+	global.process = Object.assign(_process, {
+		stdin: Object.assign(_process.stdin, {
+			on: (value, cb) => {
+				cb(inValue);
+			},
+		}),
+	});
 };
 
 describe('commandListener', () => {
-	it('execute without error', done => {
+	it('write result in buffer', done => {
+		addString('test');
+		global.process = Object.assign(_process, {
+			stdout: Object.assign(_process.stdout, {
+				write: value => {
+					if (value !== '') {
+						expect(value).toBe('test');
+						done();
+					}
+				},
+			}),
+		});
+		expect(commandListener(() => {})).toBe(undefined);
+	});
+	it('send command by press enter', done => {
 		global.process = Object.assign(_process, {
 			stdin: Object.assign(_process.stdin, {
 				on: (value, cb) => {
-					expect(value).toBe('readable');
-					cb();
+					expect(value).toBe('keypress');
+					cb('', {
+						ctrl: false,
+						name: 'return',
+					});
 				},
 			}),
 		});
@@ -29,18 +61,59 @@ describe('commandListener', () => {
 			})
 		).toBe(undefined);
 	});
-	it('execute without error when chunk is null', () => {
+	it('press enter but without anything entered', () => {
+		expect(commandListener(() => {})).toBe(undefined);
+	});
+	it('press ctrl c', done => {
+		addString('test');
 		global.process = Object.assign(_process, {
 			stdin: Object.assign(_process.stdin, {
 				on: (value, cb) => {
-					expect(value).toBe('readable');
-					cb();
-				},
-				read() {
-					return null;
+					expect(value).toBe('keypress');
+					cb('c', {
+						ctrl: true,
+						name: 'c',
+					});
 				},
 			}),
 		});
+		global.process = Object.assign(_process, {
+			exit: () => {
+				done();
+			},
+		});
+		expect(commandListener(() => {})).toBe(undefined);
+	});
+	it('write result in buffer', () => {
+		addString('test');
+		expect(commandListener(() => {})).toBe(undefined);
+	});
+	it('press backspace', done => {
+		addString('test');
+		global.process = Object.assign(_process, {
+			stdin: Object.assign(_process.stdin, {
+				on: (value, cb) => {
+					cb('', {
+						ctrl: false,
+						name: 'backspace',
+					});
+				},
+			}),
+		});
+		global.process = Object.assign(_process, {
+			stdout: Object.assign(_process.stdout, {
+				write: value => {
+					if (value[0] === 't') {
+						expect(value).toBe('tes');
+						done();
+					}
+				},
+			}),
+		});
+		expect(commandListener(() => {})).toBe(undefined);
+	});
+	it('press undefined key', () => {
+		addString(undefined);
 		expect(commandListener(() => {})).toBe(undefined);
 	});
 });
