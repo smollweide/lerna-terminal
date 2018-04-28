86cf1a51463eae4b4b356851aca7a07740525545
Simon Mollweide <simon.mollweide@namics.com>
2018-04-28 16:26:58 +0200

    refactor(command-listener): optimize stdin keypress handling

--- src/commandListener/index.js
+++ src/commandListener/index.js
@@ -8,70 +8,104 @@ let buffer = '';
 const history = [];
 let currentSelectedHistory = 0;
 
+const onCtrlC = () => {
+	buffer = '';
+	process.exit();
+};
+
+const onBackspace = () => {
+	buffer = buffer.substring(0, buffer.length - 1);
+	getUiState().onChange(buffer);
+};
+
+const onUp = () => {
+	const uiState = getUiState();
+	if (history[currentSelectedHistory]) {
+		buffer = history[currentSelectedHistory];
+		uiState.onChange(buffer);
+	} else {
+		buffer = '';
+		uiState.onChange(buffer);
+	}
+
+	if (currentSelectedHistory >= history.length) {
+		currentSelectedHistory = 0;
+	} else {
+		currentSelectedHistory += 1;
+	}
+};
+
+const onDown = () => {
+	const uiState = getUiState();
+	if (currentSelectedHistory >= history.length - 1) {
+		currentSelectedHistory = history.length - 2;
+	} else if (currentSelectedHistory <= -1) {
+		currentSelectedHistory = -1;
+	} else {
+		currentSelectedHistory -= 1;
+	}
+
+	if (history[currentSelectedHistory]) {
+		buffer = history[currentSelectedHistory];
+		uiState.onChange(buffer);
+	} else {
+		buffer = '';
+		uiState.onChange(buffer);
+	}
+};
+
+const onReturn = onCommandEntered => {
+	if (buffer.length > 0) {
+		onCommandEntered(
+			buffer
+				.trim()
+				.replace(/\n/g, '')
+				.replace(/\t/g, '')
+		);
+		history.reverse();
+		history.push(buffer);
+		history.reverse();
+	}
+	buffer = '';
+	currentSelectedHistory = 0;
+	getUiState().onChange('');
+};
+
+const onRecieveLetter = letter => {
+	buffer += letter;
+	getUiState().onChange(buffer);
+};
+
+const keyNameMap = {
+	backspace: onBackspace,
+	up: onUp,
+	down: onDown,
+	return: onReturn,
+};
+
 /**
  * @param {Function<string>} onCommandEntered - the callback function
  * @returns {void}
  **/
 function commandListener(onCommandEntered) {
-	const uiState = getUiState();
-
 	process.stdin.setEncoding('utf8');
 	process.stdin.setRawMode(true);
 	process.stdin.on('keypress', (letter, key) => {
-		if (key && key.ctrl && key.name === 'c') {
-			buffer = '';
-			process.exit();
-		} else if (key && key.name === 'backspace') {
-			buffer = buffer.substring(0, buffer.length - 1);
-			uiState.onChange(buffer);
-		} else if (key && key.name === 'up') {
-			if (history[currentSelectedHistory]) {
-				buffer = history[currentSelectedHistory];
-				uiState.onChange(buffer);
-			} else {
-				buffer = '';
-				uiState.onChange(buffer);
+		if (!key && letter) {
+			onRecieveLetter(letter);
+			return;
+		}
+
+		if (key) {
+			if (key.ctrl && key.name) {
+				onCtrlC();
+				return;
 			}
 
-			if (currentSelectedHistory >= history.length) {
-				currentSelectedHistory = 0;
-			} else {
-				currentSelectedHistory += 1;
+			if (keyNameMap[key.name]) {
+				keyNameMap[key.name](onCommandEntered);
+				return;
 			}
-		} else if (key && key.name === 'down') {
-			if (currentSelectedHistory >= history.length - 1) {
-				currentSelectedHistory = history.length - 2;
-			} else if (currentSelectedHistory <= -1) {
-				currentSelectedHistory = -1;
-			} else {
-				currentSelectedHistory -= 1;
-			}
-
-			if (history[currentSelectedHistory]) {
-				buffer = history[currentSelectedHistory];
-				uiState.onChange(buffer);
-			} else {
-				buffer = '';
-				uiState.onChange(buffer);
-			}
-		} else if (key && key.name === 'return') {
-			if (buffer.length > 0) {
-				onCommandEntered(
-					buffer
-						.trim()
-						.replace(/\n/g, '')
-						.replace(/\t/g, '')
-				);
-				history.reverse();
-				history.push(buffer);
-				history.reverse();
-			}
-			buffer = '';
-			currentSelectedHistory = 0;
-			uiState.onChange('');
-		} else if (letter) {
-			buffer += letter;
-			uiState.onChange(buffer);
 		}
 	});
 	process.stdin.resume();
